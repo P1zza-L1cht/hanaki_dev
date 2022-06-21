25286169b7c9fc5baf566f93f6d3ea47dec01ed7
P1zza-L1cht <haruki21corder@gmail.com>
2022-06-21 22:56:52 +0900

    base

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import './App.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/setting' element={<Setting />} />
+          <Route path='*' element={<Error />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
