534299d07c9471509f386adaed7d0e27ec49e4f8
P1zza-L1cht <haruki21corder@gmail.com>
2022-07-05 13:54:00 +0900

    footer layout

--- components/Footer.js
+++ components/Footer.js
@@ -1,15 +1,20 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 export default function Footer() {
   return (
-    <footer className="w-full h-[150px] bg-red-300">
-      <div className="">
-        <h4 className="">Social Media</h4>
-        <div className="w-4 h-4">
-        <FontAwesomeIcon icon={faTwitter} />
+    <footer className="w-full h-[135px] bg-red-300 relative">
+      <div className="flex flex-col items-center py-2 px-3">
+        <h4 className="text-xl font-bold">Social Media</h4>
+        <div className="h-[32px] w-full flex items-center justify-center pt-2">
+          <a href="https://mobile.twitter.com/rin_vt32" className='text-blue-400 text-xl px-1'>
+            <FontAwesomeIcon icon={faTwitter} className="fa-xl"/>
+          </a>
+          <a href="https://www.youtube.com/channel/UCLogCKK4LrSGyRirmfIBl1w" className='text-red-600 text-xl px-1'>
+            <FontAwesomeIcon icon={faYoutube} className="fa-xl" />
+          </a>
         </div>
-        <p>🄫Copyright All rights reserved by Hanaki Rin</p>
+        <p className='absolute bottom-2'>🄫Copyright All rights reserved by Hanaki Rin</p>
       </div>
     </footer>
   );
