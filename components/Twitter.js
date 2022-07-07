57d4bd2e8ed15a5826a6dd19da38a5826e8a6fb5
P1zza-L1cht <haruki21corder@gmail.com>
2022-07-07 19:25:07 +0900

    twitter timeline hooks fixed

--- components/Twitter.js
+++ components/Twitter.js
@@ -1,7 +1,21 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { useEffect } from 'react';
 
 export default function Twitter() {
+
+  useEffect(() => {
+    if(document.getElementById("twt")){
+      const removeTWT = document.getElementById("twt");
+      removeTWT.remove();
+    }
+    const twt = document.createElement("script");
+    twt.setAttribute("src", "https://platform.twitter.com/widgets.js");
+    twt.setAttribute("async", "true");
+    twt.setAttribute("id", "twt");
+    document.head.appendChild(twt);
+  }, []);
+
   return (
     <div className="h-[500px] lg:h-[800px] w-full bg-sky-200 ">
       <div className="flex items-center w-full h-[10%]">
@@ -21,7 +35,6 @@ export default function Twitter() {
       </div>
       <div className="h-[85%] w-[90%] md:w-[70%] mx-auto overflow-y-auto">
         <a className="twitter-timeline" href="https://twitter.com/rin_vt32?ref_src=twsrc%5Etfw">Tweets by rin_vt32</a>
-        <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
       </div>
     </div>
   )
