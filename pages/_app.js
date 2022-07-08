0723abc0d13c7861df9531a83cacf1a5af2094af
P1zza-L1cht <haruki21corder@gmail.com>
2022-07-08 23:14:43 +0900

    reset systems and separate to prod and dev

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,11 @@
 import '../styles/globals.css'
-import { SessionProvider } from "next-auth/react"
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 
-function MyApp({ Component, pageProps: {session, ...pageProps} }) {
+function MyApp({ Component, pageProps }) {
   return(
-    <SessionProvider session={session} >
-      <Component {...pageProps} />
-    </SessionProvider>
+    <Component {...pageProps} />
   );
 }
 
