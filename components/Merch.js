e14939b23f5237c381652bc52ce5c6b35eba82c1
P1zza-L1cht <haruki21corder@gmail.com>
2022-07-10 22:08:38 +0900

    merch items fetched from firebase

--- components/Merch.js
+++ components/Merch.js
@@ -1,7 +1,21 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCompactDisc } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from 'react';
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { db } from '../firebase';
+import Moment from 'react-moment';
+
 
 export default function Merch() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => onSnapshot(
+    query(collection(db, "merchandises"), orderBy("timestamp", "desc")),
+    (snapshot) => {
+      setItems(snapshot.docs);
+    }
+  ), []);
+
   return (
     <div className="h-[500px] lg:h-[800px] w-full bg-rose-200 px-3 py-2">
       <div className="flex items-center justify-center my-2">
@@ -12,19 +26,29 @@ export default function Merch() {
       </div>
       <div className="w-full h-[90%] overflow-y-auto mt-3 flex flex-row flex-wrap ">
 
-        {/* merch card */}
-        <div className="flex flex-col items-center w-[30%] h-[30%] bg-rose-100 p-2 rounded-lg">
-          <div className="flex items-center justify-center">
-            <img src="/images/hanaki_def.png" alt="Merchandise image" width="60%" height="60%" />
+      {items.map((item) => (
+        <a
+          href={item?.data()?.site}
+          className="flex flex-col items-center w-[30%] h-[40%] bg-rose-100 p-2 rounded-lg"
+          key={item.id}
+        >
+          <div className="flex items-center justify-center mt-6">
+            {item.data().image && (
+              <img src={item?.data()?.image} alt="Merchandise image" width="60%" height="60%" />
+            )}
+            {!item.data().image && (
+              <img src="/images/noImage.png" alt="Merchandise image" width="60%" height="60%" />
+            )}
           </div>
-          <div className="flex">
-            <h3>Title</h3>
-            <p>date</p>
+          <div className="flex mt-3">
+            <h3>{item?.data()?.title}</h3>
+            <Moment format="YYYY/MM/DD" className='mx-2'>{item?.data()?.timestamp?.toDate()}</Moment>
           </div>
-          <div className="">
-            <p>content text</p>
+          <div className="mt-1">
+            <p>{item?.data()?.content}</p>
           </div>
-        </div>
+        </a>
+      ))}
       </div>
     </div>
   )
